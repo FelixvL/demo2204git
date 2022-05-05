d34d403622263ed23453271ee097d8c12d4eee80
FelixvL <digitaleservice@gmail.com>
2022-05-05 14:11:30 +0200

    heel veel bij wegaanbeginnen.py

--- wegaanbeginnen.py
+++ wegaanbeginnen.py
@@ -1,3 +1,20 @@
+from ssl import VERIFY_CRL_CHECK_LEAF
+
+
+hoi hier gaan we typen
+print("dit is python")
+
+nog iets
+
+
+HEELL VEEL
+
+
+hoi hier gaan we typen
+print("dit is python")
+
+nog iets
+
 hoi hier gaan we typen
 print("dit is python")
 
